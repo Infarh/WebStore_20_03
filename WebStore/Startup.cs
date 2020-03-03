4b41cfdf61d2c9922628f69dfa00394db2417769
Infarh <shmachilin@yandex.ru>
2020-03-03 20:27:18 +0300

    Пример работы с конфигурацией

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -27,11 +27,13 @@ namespace WebStore
 
             app.UseRouting();
 
+            var greetings = Configuration["CustomGreetings"];
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync(greetings);
                 });
             });
         }
