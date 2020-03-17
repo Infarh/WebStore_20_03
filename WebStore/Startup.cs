2801b28ea52b22ac5c576223d3051cf621c74001
Infarh <shmachilin@yandex.ru>
2020-03-17 20:45:52 +0300

    Рефакторинг сервисов хранения данных в памяти

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -13,6 +13,7 @@ using WebStore.DAL.Context;
 using WebStore.Data;
 using WebStore.Infrastructure.Interfaces;
 using WebStore.Infrastructure.Services;
+using WebStore.Infrastructure.Services.InMemory;
 
 namespace WebStore
 {
