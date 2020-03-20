afc18a1caa75a3bb4457e123509d2d2930a26b25
Infarh <shmachilin@yandex.ru>
2020-03-20 21:11:13 +0300

    Реализация механизма входа пользвоателя в систему

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -43,12 +43,43 @@ namespace WebStore.Controllers
                 ModelState.AddModelError(string.Empty, error.Description);
 
             return View(Model);
+        }
+
+        #endregion
+
+        #region Вход пользователя в систему
+
+        public IActionResult Login(string ReturnUrl) => View(new LoginViewModel { ReturnUrl = ReturnUrl });
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginViewModel Model)
+        {
+            if (!ModelState.IsValid) return View(Model);
+
+            var login_result = await _SignInManager.PasswordSignInAsync(
+                Model.UserName,
+                Model.Password,
+                Model.RememberMe,
+                false);
+
+            if (login_result.Succeeded)
+            {
+                if (Url.IsLocalUrl(Model.ReturnUrl))
+                    return Redirect(Model.ReturnUrl);
+                return RedirectToAction("Index", "Home");
+            }
+
+            ModelState.AddModelError(string.Empty, "Неверное имя пользователя, или пароль");
+
+            return View(Model);
         } 
 
         #endregion
 
-        public IActionResult Login() => View(new LoginViewModel());
-
-        public IActionResult Logout() => RedirectToAction("Index", "Home");
+        public async Task<IActionResult> Logout()
+        {
+            await _SignInManager.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
