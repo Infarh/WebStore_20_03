fe81c98cf685daf8a626c2bded5d24362c7a4f24
Infarh <shmachilin@yandex.ru>
2020-03-20 20:39:44 +0300

    Логика регистрации пользователя в системе

--- WebStore/Controllers/AccountController.cs
+++ WebStore/Controllers/AccountController.cs
@@ -1,11 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain.Entities.Identity;
 using WebStore.ViewModels.Identity;
 
 namespace WebStore.Controllers
 {
     public class AccountController : Controller
     {
+        private readonly UserManager<User> _UserManager;
+        private readonly SignInManager<User> _SignInManager;
+
+        public AccountController(UserManager<User> UserManager, SignInManager<User> SignInManager)
+        {
+            _UserManager = UserManager;
+            _SignInManager = SignInManager;
+        }
+
+        #region Регистрация пользователя в системе
+
         public IActionResult Register() => View(new RegisterUserViewModel());
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public async Task<IActionResult> Register(RegisterUserViewModel Model)
+        {
+            if (!ModelState.IsValid)
+                return View(Model);
+
+            var user = new User
+            {
+                UserName = Model.UserName
+            };
+
+            var register_result = await _UserManager.CreateAsync(user, Model.Password);
+            if (register_result.Succeeded)
+            {
+                await _SignInManager.SignInAsync(user, false);
+                return RedirectToAction("Index", "Home");
+            }
+
+            foreach (var error in register_result.Errors)
+                ModelState.AddModelError(string.Empty, error.Description);
+
+            return View(Model);
+        } 
+
+        #endregion
 
         public IActionResult Login() => View(new LoginViewModel());
     }
