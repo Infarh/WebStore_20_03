257cc8d0d1f9dd37f87da12ad97851a9bee2ac4b
Infarh <shmachilin@yandex.ru>
2020-03-27 19:47:37 +0300

    Область администратора - развитие

--- WebStore/Startup.cs
+++ WebStore/Startup.cs
@@ -100,13 +100,13 @@ namespace WebStore
                 });
 
                 endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
-
-                endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                 );
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
