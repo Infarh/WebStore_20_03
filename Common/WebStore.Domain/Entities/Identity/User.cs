4d5ff5872014a786cb09eb31293cae86816eb6c4
Infarh <shmachilin@yandex.ru>
2020-04-07 21:23:29 +0300

    Устанавливаем номер версии нашего WebAPI = v1

--- Common/WebStore.Domain/Entities/Identity/User.cs
+++ Common/WebStore.Domain/Entities/Identity/User.cs
@@ -4,7 +4,7 @@ namespace WebStore.Domain.Entities.Identity
 {
     public class User : IdentityUser
     {
-        public const string Administrator = "Admin";
+        public const string Administrator = "Administrator";
         public const string AdminDefaultPassword = "AdminPassword";
     }
 }
--- Common/WebStore.Domain/WebAPI.cs
+++ Common/WebStore.Domain/WebAPI.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace WebStore.Domain
+﻿namespace WebStore.Domain
 {
     public static class WebAPI
     {
-        public const string Employees = "api/employees";
+        public const string Employees = "api/v1/employees";
 
-        public const string Products = "api/products";
+        public const string Products = "api/v1/products";
 
-        public const string Orders = "api/orders";
+        public const string Orders = "api/v1/orders";
     }
 }
