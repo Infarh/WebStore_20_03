f09ffa0ff912916e6db2962fcc299aa801dfe5be
Infarh <shmachilin@yandex.ru>
2020-04-07 20:40:37 +0300

    Контроллер WebAPI каталога товаров

--- Common/WebStore.Domain/WebAPI.cs
+++ Common/WebStore.Domain/WebAPI.cs
@@ -7,5 +7,7 @@ namespace WebStore.Domain
     public static class WebAPI
     {
         public const string Employees = "api/employees";
+
+        public const string Products = "api/products";
     }
 }
