1f106f61af0cb02ae83f0c00d0dcad51859d4f31
Infarh <shmachilin@yandex.ru>
2020-04-07 21:08:27 +0300

    Контроллер WebAPI заказа

---
+++ Services/WebStore.ServiceHosting/Controllers/OrdersApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Domain;
+using WebStore.Domain.DTO.Orders;
+using WebStore.Domain.Entities.Orders;
+using WebStore.Interfaces.Services;
+
+namespace WebStore.ServiceHosting.Controllers
+{
+    [Route(WebAPI.Orders)]
+    [ApiController]
+    public class OrdersApiController : ControllerBase, IOrderService
+    {
+        private readonly IOrderService _OrderService;
+
+        public OrdersApiController(IOrderService OrderService) => _OrderService = OrderService;
+
+        [HttpGet]
+        public IEnumerable<OrderDTO> GetUserOrders(string UserName) => _OrderService.GetUserOrders(UserName);
+
+        public OrderDTO GetOrderById(int id) => _OrderService.GetOrderById(id);
+
+        public Task<OrderDTO> CreateOrderAsync(string UserName, CreateOrderModel OrderModel) => _OrderService.CreateOrderAsync(UserName, OrderModel);
+    }
+}
