ebde232c6f04bd8e85893a1c93a4ce6e02fb8ae2
Infarh <shmachilin@yandex.ru>
2020-04-17 20:02:34 +0300

    Исправляем расположение контроллера WebAPITestController

---
+++ Tests/WebStore.Tests/Controllers/WebAPITestControllerTests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Assert = Xunit.Assert;
+
+namespace WebStore.Tests.Controllers
+{
+    [TestClass]
+    public class WebAPITestControllerTests
+    {
+
+    }
+}
